d96faa832003b3bbb8847670e535e1d5c12597e7
Casey Collins <Caseyc585@users.noreply.github.com>
2016-12-22 20:05:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Output from task 1');
+var app = {
+    Received: function () {
+        console.log("Received");
+    },
+    Updated: function () {
+        console.log("Updated");
+    },
+    Prep: function () {
+        console.log("Prep");
+    },
+    Start: function () {
+        console.log("Start");
+        for (var i = 0; i < 5; i++) {
+            console.log('Test Task 1 output ' + i);
+        }
+    },
+    Completed: function () {
+        console.log("Completed");
+    },
+    Removed: function () {
+        console.log("Removed");
+    }
+}
+
+module.exports = app;
